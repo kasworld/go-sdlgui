4c0066ab286825530f22285393192d7eca24c17e
SeukWon Kang <kasworld@gmail.com>
2015-02-21 17:40:56 +0900

    code clean, update doc

--- README.md
+++ README.md
@@ -16,3 +16,20 @@ https://github.com/veandco/go-sdl2
 ## korean discription
 
 http://kasw.blogspot.kr/2015/02/go-gui.html
+
+## Requirements2
+
+	"github.com/kasworld/idgen"
+	"github.com/kasworld/log"
+	"github.com/kasworld/quadtree"
+	"github.com/kasworld/rect"
+	"github.com/kasworld/htmlcolors"
+
+## Requirements for example 
+
+	"github.com/kasworld/actionstat"
+	"github.com/kasworld/htmlcolors"
+	"github.com/kasworld/log"
+
+	"github.com/kasworld/go-sdlgui"
+	
--- example/example.go
+++ example/example.go
@@ -18,55 +18,55 @@ func main() {
 
 	app := App{
 		Stat:  actionstat.NewActionStat(),
-		sdlCh: make(chan interface{}, 1),
+		SdlCh: make(chan interface{}, 1),
+		Keys:  make(map[sdl.Scancode]bool),
 	}
-	app.Keys = make(map[sdl.Scancode]bool)
 	app.Run()
 	runtime.UnlockOSThread()
 }
 
 type App struct {
 	Quit  bool
-	sdlCh chan interface{}
+	SdlCh chan interface{}
 	Keys  sdlgui.KeyState
+	Win   *sdlgui.Window
 	Stat  *actionstat.ActionStat
 
-	window   *sdlgui.Window
 	controls []sdlgui.ControlI
 	msgtexts *sdlgui.TextBoxControl
 	barctrl  *sdlgui.TextControl
 }
 
 func (g *App) addControls() {
-	g.window = sdlgui.NewWindow("", 1024, 800, true)
+	g.Win = sdlgui.NewWindow("", 1024, 800, true)
 
 	g.msgtexts = sdlgui.NewTextBoxControl(
 		0, 0, 0,
 		1024, 720, 60,
 		sdlgui.LoadFont("DejaVuSerif.ttf", 12))
 	g.msgtexts.SetBG(htmlcolors.Gray.ToRGBA())
-	g.window.AddControl(g.msgtexts)
+	g.Win.AddControl(g.msgtexts)
 
 	g.barctrl = sdlgui.NewTextControl(
 		0, 720, 0,
 		1024, 80, "hello",
 		sdlgui.LoadFont("DejaVuSerif.ttf", 36))
 	g.barctrl.SetBG(htmlcolors.Pink.ToRGBA())
-	g.window.AddControl(g.barctrl)
+	g.Win.AddControl(g.barctrl)
 
-	g.window.UpdateAll()
+	g.Win.UpdateAll()
 }
 
 func (g *App) Run() {
 	g.addControls()
-	sdlgui.SDLEvent2Ch(g.sdlCh)
+	sdlgui.SDLEvent2Ch(g.SdlCh)
 	timerInfoCh := time.Tick(time.Duration(1000) * time.Millisecond)
 	timerDrawCh := time.Tick(time.Duration(1000/60) * time.Millisecond)
 	barlen := 0.0
 	for !g.Quit {
 		select {
-		case data := <-g.sdlCh:
-			if g.window.ProcessSDLMouseEvent(data) ||
+		case data := <-g.SdlCh:
+			if g.Win.ProcessSDLMouseEvent(data) ||
 				g.Keys.ProcessSDLKeyEvent(data) {
 				g.Quit = true
 			}
@@ -81,7 +81,7 @@ func (g *App) Run() {
 		case <-timerDrawCh:
 			g.msgtexts.DrawSurface()
 			g.barctrl.DrawSurface()
-			g.window.Update()
+			g.Win.Update()
 
 		case <-timerInfoCh:
 			log.Info("stat %v", g.Stat)
